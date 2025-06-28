f9e0151774d8c1f9f06305a6e06dd809377d8808
user.name like : "sami031364477@gmail.com <sami03136424477@gmail.com>
2025-06-28 22:51:02 +0500

    add slider with images

---
+++ Travel-web/script.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 30,
+  centeredSlides: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
